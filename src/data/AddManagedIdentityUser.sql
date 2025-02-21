a668ed577aed25b977e499eacb2999a230c09ef0
Pete Tian <pete.tian@gmail.com>
2025-02-21 11:40:09 -0800

    change managed identity for access SQL server

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-llrjxxxuc3lw2 FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-llrjxxxuc3lw2;
+ALTER ROLE db_datawriter ADD MEMBER id-llrjxxxuc3lw2;
 
